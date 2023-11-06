78ab8795da4504dc8a6a5dc47d79250e37ff78ba
Yus11 <67752893+Yus11@users.noreply.github.com>
2023-11-07 01:23:52 +0300

    Update database.js
    sfff

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', 'dpg-cl4g8782650c73a50km0-a'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'sample_wjjk'),
-        user: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', '8yoES2j6eLsRsVKGYwp4VG4COWuKWQ0G'),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), },
       },
     },
