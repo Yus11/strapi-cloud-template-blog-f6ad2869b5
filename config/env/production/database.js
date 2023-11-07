943817453c3f2d8f58d95aaf974d231714ae6d94
Yussuf <ismoiliyusuff@gmail.com>
2023-11-07 03:06:08 +0300

    ojjjkjk

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,13 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    connection: {
-      client: "postgres",
+module.exports = ({ env }) => ({
+  connection: {
+      client: 'postgres',
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password
+        host: env('DATABASE_HOST', 'dpg-cl4g8782650c73a50km0-a'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'sample_wjjk'),
+        user: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', '8yoES2j6eLsRsVKGYwp4VG4COWuKWQ0G'),
+        ssl: env.bool('DATABASE_SSL', true),
       },
-      debug: false,
     },
-  };
-};
+});
